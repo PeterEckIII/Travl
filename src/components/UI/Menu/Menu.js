13efb7fb6c997c84f24b2b21a29d50cb2f35548a
Peter Eck <jpeckiii@gmail.com>
2018-11-12 08:58:08 -0600

    Add slideIn and slideOut for Menu sidebar

--- src/components/UI/Menu/Menu.js
+++ src/components/UI/Menu/Menu.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-// import MenuChoices from "./MenuChoices/MenuChoices";
 import MenuIcon from "./MenuIcon/MenuIcon";
 import classes from "./Menu.module.css";
-// import Aux from "../../../hoc/Aux/Aux";
+
 class Menu extends Component {
     state = {
         menuIsOpen: false,
     }
 
     openMenuToggler = () => {
+        console.log(this.state.menuIsOpen);
         this.setState({
             menuIsOpen: !this.state.menuIsOpen
         });
     }
 
     render() {
+        let attachedClasses;
+        if ( this.state.menuIsOpen ) {
+            attachedClasses = [classes.Menu, classes.Opened];
+        }
+        else if ( !this.state.menuIsOpen ){
+            attachedClasses = [classes.Menu, classes.Closed];
+        }
+
         return (
-            <div className={classes.Menu}>
-                <MenuIcon onClick={ this.openMenuToggler } />
+            <div className={ attachedClasses.join(" ") }>
+                <MenuIcon clicked={ this.openMenuToggler } />
             </div>  
         )
     }
--- src/components/UI/Menu/MenuIcon/MenuIcon.js
+++ src/components/UI/Menu/MenuIcon/MenuIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from "./MenuIcon.module.css";
 
 const MenuIcon = ( props ) => (
-    <div className={classes.MenuIconContainer}>
+    <div className={classes.MenuIconContainer} onClick={props.clicked}>
         <div className={classes.MenuIcon}></div>
         <div className={classes.MenuIcon}></div>
         <div className={classes.MenuIcon}></div>
