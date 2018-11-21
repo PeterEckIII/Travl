6b201a434f58e0288f19191c2b6ed8d4a64799de
Peter Eck <jpeckiii@gmail.com>
2018-11-21 11:59:10 -0600

    Minor adjustments

--- src/components/UI/Menu/Menu.js
+++ src/components/UI/Menu/Menu.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import MenuIcon from "./MenuIcon/MenuIcon";
 import classes from "./Menu.module.css";
 
 class Menu extends Component {
-    state = {
-        menuIsOpen: false,
+  state = {
+    menuIsOpen: false
+  };
+
+  openMenuToggler = () => {
+    console.log(this.state.menuIsOpen);
+    this.setState({
+      menuIsOpen: !this.state.menuIsOpen
+    });
+  };
+
+  render() {
+    let attachedClasses;
+    if (this.state.menuIsOpen) {
+      attachedClasses = [classes.Menu, classes.Opened];
+    } else if (!this.state.menuIsOpen) {
+      attachedClasses = [classes.Menu, classes.Closed];
     }
 
-    openMenuToggler = () => {
-        console.log(this.state.menuIsOpen);
-        this.setState({
-            menuIsOpen: !this.state.menuIsOpen
-        });
-    }
-
-    render() {
-        let attachedClasses;
-        if ( this.state.menuIsOpen ) {
-            attachedClasses = [classes.Menu, classes.Opened];
-        }
-        else if ( !this.state.menuIsOpen ){
-            attachedClasses = [classes.Menu, classes.Closed];
-        }
-
-        // TODO: Conditionally render the Menu choices depending on whether the Menu is open or closed
-        return (
-            <div className={ attachedClasses.join(" ") }>
-                <MenuIcon clicked={ this.openMenuToggler } />
-            </div>  
-        )
-    }
+    // TODO: Conditionally render the Menu choices depending on whether the Menu is open or closed
+    return (
+      <div className={attachedClasses.join(" ")}>
+        <MenuIcon clicked={this.openMenuToggler} />
+      </div>
+    );
+  }
 }
 
 export default Menu;
